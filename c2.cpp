7d88353f34c27d8531cf0e5f700248b02aacaab3
45acplover <scarpu.simone@gmail.com>
2024-12-12 09:35:02 +0000

    boh2

---
+++ c2.cpp
@@ -0,0 +1,17 @@
+#include <iostream> /
+using namespace std; 
+
+
+class Rettangolo{
+    private:
+    double altezza;
+    double base;
+
+
+    public:
+
+    void setaltezza(double x){altezza + x;}
+    void setbase(double y){base + y;}
+
+
+};
